f41778df20ca4d7d92490832a84365785183d290
Mohizanur <rco47840@gmail.com>
2025-05-22 10:22:06 +0300

    Updated

--- app.js
+++ app.js
@@ -60,5 +60,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`✅ Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
